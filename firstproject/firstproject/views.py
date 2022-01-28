aac43a4e5e48ee4b324264ee9b402fe5fcc2e928
Niloy <nkundu191071@gmail.com>
2022-01-28 10:54:30 +0600

    Learn Django

--- firstproject/firstproject/views.py
+++ firstproject/firstproject/views.py
@@ -1,7 +1,19 @@
-
-
 from django.shortcuts import redirect
 from django.shortcuts import render, HttpResponse
 
+
 def home(request):
-    return render(request, 'home.html')
+    name = ['niloy', 'nipa', 'puja', 'nirjon']
+    context = {
+        'name' : name,
+    }
+    return render(request, 'home.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        content = request.POST['content']
+
+    return render(request, 'contact.html')
--- firstproject/tuition/models.py
+++ firstproject/tuition/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=17)
+    content = models.TextField()
